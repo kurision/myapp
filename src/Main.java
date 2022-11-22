1e13641bb38f5e7a67114dcf6d5ed5a38091a9fc
Kurision <kumalanil34@gmail.com>
2022-11-22 07:41:02 +0545

    kumle

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import package1.nclass;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        System.out.println("Hello world!");
+
+    }
+
+}
---
+++ src/package1/nclass.java
@@ -0,0 +1,9 @@
+package package1;
+
+public class nclass {
+     String as = " sudip is gay";
+     public static void main(String[] args){
+         nclass obj = new nclass();
+         obj.as= "welcome";
+     }
+}
